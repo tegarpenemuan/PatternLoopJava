7d9f14b7286db6f32ac41548f0d6e060c0dffd4e
Tegar Penemuan <tegarpenemuan@gmail.com>
2022-02-14 17:41:42 +0700

    Pattern18

---
+++ Pattern18/src/com/tegarpenemuan/project/Pattern18.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.tegarpenemuan.project;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author tegar
+ */
+public class Pattern18 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args)
+    {
+        // Create a new Scanner object
+        Scanner scanner = new Scanner(System.in);
+
+        // Get the number of rows from the user
+        System.out.println("Enter the number of rows needed to print the pattern ");
+
+        int rows = scanner.nextInt();
+        System.out.println("## Printing the pattern ##");
+        
+        for (int i=1; i<=rows; i++)
+        {
+            // Print space in increasing order
+            for (int j=1; j<i; j++)
+            {
+                System.out.print(" ");
+            }
+            // Print star in decreasing order
+            for (int k=rows; k>=i; k--)
+            {
+                if( i == 1 || k == i || k == rows)
+                    System.out.print("*");
+                else
+                    System.out.print(" ");
+            }
+            System.out.println();
+        }
+        scanner.close();
+    }
+    
+}
