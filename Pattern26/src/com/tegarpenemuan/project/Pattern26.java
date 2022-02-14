7ce8a7e2b7218851c6e04bab2199b378bcc2564b
Tegar Penemuan <tegarpenemuan@gmail.com>
2022-02-14 17:43:34 +0700

    Pattern26

---
+++ Pattern26/src/com/tegarpenemuan/project/Pattern26.java
@@ -0,0 +1,68 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.tegarpenemuan.project;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author tegar
+ */
+public class Pattern26 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args)
+    {
+        // Create a new Scanner object
+        Scanner scanner = new Scanner(System.in);
+
+        // Get the number of rows from the user
+        System.out.println("Enter the number of rows needed to print the pattern ");
+
+        int rows = scanner.nextInt();
+        System.out.println("## Printing the pattern ##");
+        
+        // Print i number of stars
+        for (int i=1; i<=rows; i++)
+        {
+            for (int j = 1; j <= i; j++)
+            {
+                System.out.print("*");
+            }
+            for (int k = i*2; k <= rows*2-1; k++)
+            {
+                System.out.print(" ");
+            }
+            for (int l = 1; l <= i; l++)
+            {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        
+        for (int i=1; i<=rows-1; i++)
+        {
+            for (int j = rows-1; j >= i; j--)
+            {
+                System.out.print("*");
+            }
+            for (int k = 1; k <= i*2; k++)
+            {
+                System.out.print(" ");
+            }
+            for (int l = rows-1; l >= i; l--)
+            {
+                System.out.print("*");
+            }
+            
+            System.out.println();
+        }
+        scanner.close();
+    }
+    
+}
